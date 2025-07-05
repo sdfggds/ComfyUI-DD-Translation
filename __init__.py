1039c4d4f0d51640d828cefc024f732e2f930cf1
171696257 <1761696257@qq.com>
2025-07-05 10:40:28 +0800

    更新版本号

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ from functools import lru_cache
 from aiohttp import web
 from pathlib import Path
 
-VERSION = "1.8.12"
+VERSION = "1.9.0"
 ADDON_NAME = "ComfyUI-DD-Translation"
 COMFY_PATH = Path(folder_paths.__file__).parent
 CUR_PATH = Path(__file__).parent
